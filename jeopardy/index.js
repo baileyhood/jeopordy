9c55215815c027e7af5f2eba87a6fa13d4b14d9d
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-03-28 14:57:37 -0400

    fixed refactoring

---
+++ jeopardy/index.js
@@ -0,0 +1,5 @@
+require('./controllers/homeController.js');
+require('./controllers/questionController.js');
+require('./directives/directive.js');
+require('./services/apiservice.js');
+require('./services/cacheEngine.service.js');
