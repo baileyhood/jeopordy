d50069be695dcb03e36b674a95d5ec36c73b4008
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-03-28 14:04:07 -0400

    few changes before refactoring

--- bundle.js
+++ bundle.js
@@ -31759,9 +31759,9 @@ angular
       var defer = $q.defer();
       var randomNumber = Math.floor(Math.random() * 1200);
       $http.get(url + randomNumber).then(function(data) {
-        defer.resolve(data);
+        defer.resolve(data); //review this
       });
-      return defer.promise;
+      return defer.promise; //review this too
     }
 
     function sixCat() {
@@ -31773,42 +31773,6 @@ angular
      sixCat: sixCat
    };
   });
-
-
-// angular
-//   .module('jeapordy')
-//   .service('apiService', function($http, $q, $cacheFactory) {
-//
-//     var url = 'http://jservice.io/api/category?id=';
-//     var cacheEngine = $cacheFactory ('jeapordy');
-//
-//     function getAllCategories() {
-//       var defer = $q.defer();
-//       var cache = cacheEngine.get('categories');
-//         if(cache) {
-//           defer.resolve(cache);
-//           window.glob = cache;
-//         }
-//         else {
-//         var randomNumber = Math.floor(Math.random() * 1200);
-//         $http.get(url + randomNumber).then(function(data) {
-//         cacheEngine.put('categories', data);
-//         defer.resolve(data);
-//         });
-//       }
-//       return defer.promise;
-//
-//     }
-//
-//     function sixCat() {
-//       return $q.all([getAllCategories(),getAllCategories(),getAllCategories(),getAllCategories(),getAllCategories(),getAllCategories()]);
-//     }
-//
-//    return {
-//      getAllCategories : getAllCategories,
-//      sixCat: sixCat
-//    };
-//   });
 
 },{}],10:[function(require,module,exports){
 angular
--- services/apiservice.js
+++ services/apiservice.js
@@ -8,9 +8,9 @@ angular
       var defer = $q.defer();
       var randomNumber = Math.floor(Math.random() * 1200);
       $http.get(url + randomNumber).then(function(data) {
-        defer.resolve(data);
+        defer.resolve(data); //review this
       });
-      return defer.promise;
+      return defer.promise; //review this too
     }
 
     function sixCat() {
@@ -22,39 +22,3 @@ angular
      sixCat: sixCat
    };
   });
-
-
-// angular
-//   .module('jeapordy')
-//   .service('apiService', function($http, $q, $cacheFactory) {
-//
-//     var url = 'http://jservice.io/api/category?id=';
-//     var cacheEngine = $cacheFactory ('jeapordy');
-//
-//     function getAllCategories() {
-//       var defer = $q.defer();
-//       var cache = cacheEngine.get('categories');
-//         if(cache) {
-//           defer.resolve(cache);
-//           window.glob = cache;
-//         }
-//         else {
-//         var randomNumber = Math.floor(Math.random() * 1200);
-//         $http.get(url + randomNumber).then(function(data) {
-//         cacheEngine.put('categories', data);
-//         defer.resolve(data);
-//         });
-//       }
-//       return defer.promise;
-//
-//     }
-//
-//     function sixCat() {
-//       return $q.all([getAllCategories(),getAllCategories(),getAllCategories(),getAllCategories(),getAllCategories(),getAllCategories()]);
-//     }
-//
-//    return {
-//      getAllCategories : getAllCategories,
-//      sixCat: sixCat
-//    };
-//   });
