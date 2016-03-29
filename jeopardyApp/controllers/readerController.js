798c0e495aa852cdc8d46037a8a5bf2466134434
William Hood <williamhood@Williams-MacBook-Pro-2.local>
2016-03-29 14:50:39 -0400

    reworked my modal

--- jeopardyApp/controllers/readerController.js
+++ jeopardyApp/controllers/readerController.js
@@ -1,43 +1,39 @@
 angular
-  .module('jeapordy')
-  .controller('ReaderController', function($scope, $rootScope) {
+    .module('jeapordy')
+    .controller('ReaderController', function($scope, $rootScope) {
+        $rootScope.score = 0;
+        $scope.submitAnswer = function(userInput, actualAnswer, value) {
 
-    $rootScope.player = {
-      score: 0
-    };
+            if (userInput === actualAnswer) {
+                $rootScope.score += value * 2;
+            } else {
+                $rootScope.score -= value * 2;
+            }
+        };
 
+        $scope.disable = function(id) {
+          console.log('disabled clicky', id);
+          $('button.' + id).prop('disabled', true);
+           $('#' + id).modal('hide');
+           $('button.' + id).css({
+             "color": '#CFCFCF',
+             "text-decoration": "line-through"
+           });
+        };
+        $scope.showMe = function() {
+            $scope.show = true;
+        };
 
-  $scope.submitAnswer = function (userInput, actualAnswer, value) {
-    if (userInput === actualAnswer) {
-        $rootScope.player.score += value * 2;
-        console.log("right answer!", $scope.player.score );
-      }
-    else {
-      $rootScope.player.score -= value * 2;
-      console.log("wrong answer!", $scope.player.score);
-    }
-  };
+        $scope.showWrong = function() {
+            $scope.showwrong = true;
+        };
 
-  $scope.isDisabled = false;
+        $scope.hide = function() {
+            $scope.show = false;
+            $scope.showwrong = false;
+        };
 
-  $scope.disableButton = function() {
-        $scope.isDisabled = true;
-    };
-
-$scope.showMe = function(){
-  $scope.show=true;
-};
-
-$scope.showWrong = function () {
-  $scope.showwrong=true;
-};
-
-$scope.hide = function () {
-  $scope.show=false;
-  $scope.showwrong=false;
-};
-
-$scope.clear = function () {
-  $scope.answerinput = '';
-};
-  });
+        $scope.clear = function() {
+            $scope.answerinput = '';
+        };
+    });
